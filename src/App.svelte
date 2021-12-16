8930bcd41d11dfb7fe3172659bf558dd4caca95b
jujumong <jujumong365@gmail.com>
2021-12-16 10:30:21 +0900

    추가 : 나머지 설정

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,14 @@
 <script>
-  export let name;
-
   import { db } from "./firebaseApp";
-  import { collection, onSnapshot } from "firebase/firestore";
+  import { addDoc, collection, doc, onSnapshot } from "firebase/firestore";
   import { onMount } from "svelte";
+  import { currency } from "./utils/format";
 
   let menu = [];
 
   const menuRef = collection(db, "menu");
+  let orderId = null;
+  let currentOrder = null;
 
   onMount(() => {
     const unsubscribe = onSnapshot(menuRef, (snapshot) => {
@@ -20,7 +21,6 @@
       });
       menu = newMenu;
     });
-
     return () => {
       unsubscribe();
     };
@@ -30,14 +30,16 @@
     detail: menu.map((item) => ({
       ...item,
       count: 0,
-      total: 0,
     })),
   };
   $: filteredOrder = {
     ...order,
     detail: order.detail.filter((item) => item.count !== 0),
   };
-	$: sum = filteredOrder.detail.reduce( ( sum, item ) => sum + item.price * item.count, 0 );
+  $: sum = filteredOrder.detail.reduce(
+    (sum, item) => sum + item.price * item.count,
+    0
+  );
 
   function addItem(name) {
     const index = order.detail.findIndex((item) => item.name === name);
@@ -47,10 +49,49 @@
 
   function removeItem(name) {
     const index = order.detail.findIndex((item) => item.name === name);
-		if( order.detail[ index ].count > 0 ) {
-			order.detail[index].count -= 1;
-		}
+    if (order.detail[index].count > 0) {
+      order.detail[index].count -= 1;
+    }
     order = { ...order };
+  }
+
+  function submit() {
+    if (sum === 0) {
+      alert("상품을 담아주세요.");
+      return;
+    }
+    const ordersRef = collection(db, "orders");
+    addDoc(ordersRef, {
+      ...filteredOrder,
+      sum,
+      status: "주문완료",
+      date: new Date(),
+    })
+      .then((doc) => {
+        orderId = doc.id;
+      })
+      .catch(console.warn);
+  }
+
+  $: {
+    if (orderId) {
+      const orderRef = doc(db, `orders/${orderId}`);
+      onSnapshot(orderRef, (snapshot) => {
+        currentOrder = snapshot.data();
+      });
+    }
+  }
+  $: {
+    if (currentOrder && currentOrder.status === "픽업완료") {
+      orderId = null;
+      currentOrder = null;
+      order = {
+        detail: menu.map((item) => ({
+          ...item,
+          count: 0,
+        })),
+      };
+    }
   }
 </script>
 
@@ -59,10 +100,12 @@
   <ul>
     {#each menu as item (item.id)}
       <li>
-        {item.name} : {item.price}원
+        {item.name} : {currency(item.price)}원
 
-        <button on:click={() => removeItem(item.name)}>-</button>
-        <button on:click={() => addItem(item.name)}>+</button>
+        {#if !currentOrder}
+          <button on:click={() => removeItem(item.name)}>-</button>
+          <button on:click={() => addItem(item.name)}>+</button>
+        {/if}
       </li>
     {/each}
   </ul>
@@ -71,19 +114,23 @@
   <h2>주문서</h2>
 
   <ul>
-		{#if filteredOrder.detail.length === 0}
-			<li>주문서가 비어 있습니다.</li>
-		{/if}
+    {#if filteredOrder.detail.length === 0}
+      <li>주문서가 비어 있습니다.</li>
+    {/if}
     {#each filteredOrder.detail as item (item.name)}
       <li>
-        {item.name} &times; {item.count}잔 = {item.price * item.count}원
+        {item.name} &times; {item.count}잔 = {currency(item.price * item.count)}원
       </li>
     {/each}
   </ul>
 
-	<p>총 금액: {sum}</p>
+  <p>총 금액: {currency(sum)}</p>
 
-  <button>주문하기</button>
+  {#if currentOrder}
+    <div>상태 : {currentOrder.status}</div>
+  {:else}
+    <button on:click={submit}>주문하기</button>
+  {/if}
 </main>
 
 <style>
